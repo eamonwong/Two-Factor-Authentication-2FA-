9506cb3aee898b477690a23d6668210938130b33
eamon01 <75913022+eamonwong@users.noreply.github.com>
2023-02-10 15:55:21 +0000

    Create main,.py

---
+++ main,.py
@@ -0,0 +1,30 @@
+# steam
+import time
+import pyotp
+
+def gen_key():
+    return pyotp.random_base32()
+
+def gen_url(key: str):
+    return pyotp.totp.TOTP(key).provisioning_uri(name='Eamon', issuer_name='My 2FA App')
+
+def generate_code(key: str):
+    totp = pyotp.TOTP(key)
+    return totp.now()
+
+def verify_code(key: str, code: str):
+    totp = pyotp.TOTP(key)
+    return totp.verify(code)
+
+key = gen_key() #Put in Database
+print(key)
+uri = gen_url(key)
+print(uri)
+
+code = generate_code(key)
+print(code)
+time.sleep(15)
+code2 = generate_code(key)
+
+print(verify_code(key, code))
+print(verify_code(key, code2))
